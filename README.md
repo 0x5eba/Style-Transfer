833cf7607cd5b5fc8a7e423ae14e54dd75ad9f18
0x5eba <sbiollo@gmail.com>
2018-06-11 16:05:02 +0200

    Made clear

--- README.md
+++ README.md
@@ -1,15 +1,2 @@
 # Style-Transfer
 Style transfer of two images using Tensorflow and Keras
-
-## Example
-Input Image:
-
-<img src="https://github.com/0x5eba/Style-Transfer/blob/master/hugo.jpg" width="200" height="200">
-
-Style Image:
-
-<img src="https://github.com/0x5eba/Style-Transfer/blob/master/wave.jpg" width="200" height="200">
-
-Style Transfer Image:
-
-<img src="https://github.com/0x5eba/Style-Transfer/blob/master/result/final.jpg" width="200" height="200">
---
+++ StyleTransfer.py
@@ -0,0 +1,30 @@
+from neural_stylization.transfer_style import Stylizer
+from neural_stylization.optimizers.l_bfgs import L_BFGS
+from neural_stylization.util.build_callback import build_callback
+from neural_stylization.util.img_util import load_image
+import glob
+
+
+CONTENT = 'img/content/me.jpg'
+
+
+
+size_content = load_image(CONTENT).size
+dims = tuple([int(x/2) for x in size_content])
+
+sty = Stylizer(content_weight=1, style_weight=1e5)
+
+for path_style_img in glob.glob('img/styles/*.jpg'):
+    print(path_style_img)
+    name_style_img = path_style_img.split('/')[-1]
+    seated_nudes = sty(
+        content_path=CONTENT,
+        style_path=path_style_img,
+        optimize=L_BFGS(max_evaluations=20),
+        iterations=10,
+        image_size=dims,
+        initialization_strat='content',
+        callback=build_callback('build/me_transfer/' + name_style_img)
+    )
+
+    seated_nudes.save('img/me_transfer/' + name_style_img)
